3ece5fd85c771f204924d2848bbf8971c147c1c4
anushamaurya0407 <your_github_email@example.com>
2026-04-09 15:48:26 +0530

    Initial Commit

--- src/main/java/iOSApp.java
+++ src/main/java/iOSApp.java
@@ -1,7 +1,5 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidElement;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,10 +12,12 @@ import org.testng.annotations.Test;
 
 public class iOSApp {
 
-    String userName = System.getenv("LT_USERNAME") == null ? "username" : System.getenv("LT_USERNAME"); //Add username here
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
-    String app_id = System.getenv("LT_APP_ID") == null ? "lt://proverbial-ios" : System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
-    String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
+    String userName = "anushamtestmuai"; //Add username here
+    String accessKey = "LT_lB8vD3Dh50vYdC6tC3sExOOyvaqAnj8Ot73XsqugmMY7YB1"; //Add accessKey here
+    String app_id = "lt://APP10160251981775646233233424";
+    //String app_id = "lt://APP1016024601774620720796792";
+    // Enter your LambdaTest App ID at the place of lt://proverbial-android
+    //String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
     AppiumDriver driver;
 
@@ -27,7 +27,7 @@ public class iOSApp {
 
         try {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("build", "Java TestNG");
+            capabilities.setCapability("build", "Sample test by Anusha");
             capabilities.setCapability("name", platform + " " + device + " " + version);
             capabilities.setCapability("deviceName", device);
             capabilities.setCapability("platformVersion", version);
@@ -39,46 +39,14 @@ public class iOSApp {
             capabilities.setCapability("devicelog", true);
             //capabilities.setCapability("geoLocation", "HK");
 
-            String hub = "https://" + userName + ":" + accessKey + "@" + grid_url + "/wd/hub";
+          String hub = "https://anushamtestmuai:LT_lB8vD3Dh50vYdC6tC3sExOOyvaqAnj8Ot73XsqugmMY7YB1@mobile-hub.lambdatest.com/wd/hub";
+           // String hub = "https://tyrondemate:LT_AhbT5QoVXFa0cYoGFsLr3ZTzuS54iiOBJyPMf9WWOpSET76@mobile-hub.lambdatest.com/wd/hub";
             driver = new AppiumDriver(new URL(hub), capabilities);
 
             WebDriverWait Wait = new WebDriverWait(driver, 30);
 
             //Changes the color of the text
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("color"))).click();
-            Thread.sleep(1000);
 
-            //Changes the text to "Proverbial"
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Text"))).click();
-            Thread.sleep(1000);
-
-            //Toast will be visible
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("toast"))).click();
-            Thread.sleep(1000);
-
-            //Notification will be visible
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("notification"))).click();
-            Thread.sleep(4000);
-
-            //Opens the geolocation page
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("geoLocation"))).click();
-            Thread.sleep(4000);
-
-            //Takes back
-            driver.navigate().back();
-
-            //Takes to speedtest page
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("speedTest"))).click();
-            Thread.sleep(4000);
-            driver.navigate().back();
-
-            //Opens the browser
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Browser"))).click();
-            Thread.sleep(1000);
-            MobileElement url = (MobileElement) driver.findElementByAccessibilityId("url");
-            url.click();
-            url.sendKeys("https://www.lambdatest.com");
-            Wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("find"))).click();
             Thread.sleep(1000);
             driver.quit();
         } catch (Exception e) {
